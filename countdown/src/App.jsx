536fafe54fae89c462f1dd5e6ea81d6e3e818ee9
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-01-31 01:05:24 +0200

    react-bootstrap added

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Clock from './Clock'
 import './App.css'
+import { Form, FormControl, Button } from 'react-bootstrap'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      deadline: 'December 24, 2018',
+      deadline: '24 December 2018',
       newDeadline: ''
     }
   }
@@ -20,14 +21,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-title">Coundown to {this.state.deadline}</div>
-        <Clock 
-        deadline={this.state.deadline}
-        />  
-        <div>
-          <input placeholder='new date'
+        <Clock deadline={this.state.deadline} />  
+        <Form inline>
+          <FormControl className="Deadline-input" placeholder='new date'
           onChange={event => this.setState({newDeadline: event.target.value})}/>
-          <button onClick={() => this.changeDeadline()}>Submit</button>
-        </div>
+          <Button onClick={() => this.changeDeadline()}>Submit</Button>
+        </Form>
       </div>
     )
   }
