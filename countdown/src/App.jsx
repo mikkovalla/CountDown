f93ed5b652c4e62acddc460acccc0aee742c29ba
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-01-30 23:50:32 +0200

    input event and setState fixed

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -5,14 +5,14 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      deadline: 'December 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     }
   }
 
   changeDeadline() {
-    this.setState({
-      deadline: 'November 25, 2017'
-    })
+    console.log('state', this.state)
+    this.setState({deadline: this.state.newDeadline})
   }
 
   render() {
@@ -26,7 +26,8 @@ class App extends Component {
           <div className="Clock-seconds">20 seconds</div>
         </div>  
         <div>
-          <input placeholder='new date'/>
+          <input placeholder='new date'
+          onChange={event => this.setState({newDeadline: event.target.value})}/>
           <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
